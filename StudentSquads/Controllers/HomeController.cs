d87568750ecd90137e390321cf4e553579a1bbdf
rita <saweliewamargharita@yandex.ru>
2020-04-05 14:10:21 +0500

    Ограничила доступ. Доступ есть только к главной странице

--- StudentSquads/Controllers/HomeController.cs
+++ StudentSquads/Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 
 namespace StudentSquads.Controllers
-{
+{[AllowAnonymous]
     public class HomeController : Controller
     {
         public ActionResult Index()
