44d25b82dcd6b10a84e5cfc7d790fc322bab57e4
rita <saweliewamargharita@yandex.ru>
2020-04-07 22:24:29 +0500

    Добавила вариант с открытием формы личной информации на редактирование

--- StudentSquads/Controllers/PeopleController.cs
+++ StudentSquads/Controllers/PeopleController.cs
@@ -6,6 +6,7 @@ using System.Web;
 using System.Web.Mvc;
 using StudentSquads.Models;
 using StudentSquads.ViewModels;
+using Microsoft.AspNet.Identity;
 
 namespace StudentSquads.Controllers
 {
@@ -25,24 +26,51 @@ namespace StudentSquads.Controllers
         {
             return View();
         }
-        public ActionResult NewPerson()
+        public ActionResult PersonForm()
         {
-            var squads= _context.Squads.ToList();
+            var squads = _context.Squads.ToList();
             var viewModel = new NewPersonViewModel
             {
                 Squads = squads
             };
-            return View(viewModel);
+            //Определяю Id текущего пользователя, чтобы найти id личности
+            string id = User.Identity.GetUserId();
+            var personid = _context.Users.SingleOrDefault(u => u.Id == id).PersonId;
+            //Если User ещё не привязан к личности, возвращаем пустую форму
+            if (personid is null)
+                return View(viewModel);
+            //Иначе возвращаем заполненную
+            else
+            {
+                var person = _context.People.SingleOrDefault(p => p.Id == personid);
+                viewModel.Person = person;
+                return View(viewModel);
+            };
         }
         [HttpPost]
         public ActionResult Create(NewPersonViewModel newModel)
         {
             newModel.Person.Id = Guid.NewGuid();
             _context.People.Add(newModel.Person);
-            //Добавь ещё код изменения Usr, чтобы он ссылался на Person
+            //Добавь ещё код изменения User, чтобы он ссылался на Person
+            string id = User.Identity.GetUserId();
+            var personid = _context.Users.SingleOrDefault(u => u.Id == id).PersonId;
             _context.SaveChanges();
             
             return RedirectToAction("ShowAll","Members");
+        }
+        public ActionResult Edit(Guid id)
+        {
+            var person = _context.People.SingleOrDefault(p => p.Id == id);
+            if (person == null)
+                return HttpNotFound();
+            var viewModel = new NewPersonViewModel
+            {
+                Person = person,
+                Squads = _context.Squads.ToList()
+
+            };
+            return View("NewPerson", viewModel);
         }
 
     }
