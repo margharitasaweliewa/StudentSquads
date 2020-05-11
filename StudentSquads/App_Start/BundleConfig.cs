4976277175b570fc29360a43ca049ac168517ba5
rita <saweliewamargharita@yandex.ru>
2020-05-11 19:42:13 +0500

    Добавила оповещение о сохранение должности

--- StudentSquads/App_Start/BundleConfig.cs
+++ StudentSquads/App_Start/BundleConfig.cs
@@ -15,7 +15,8 @@ namespace StudentSquads
                         "~/Scripts/respond.js",
                         "~/Scripts/datatables/jquery.datatables.js",
                         "~/Scripts/datatables/datatables.bootstrap.js",
-                        "~/Scripts/typeahead.bundle.js"));
+                        "~/Scripts/typeahead.bundle.js",
+                        "~/Scripts/toastr.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -31,7 +32,8 @@ namespace StudentSquads
                       "~/Content/bootstrap-lumen.css",
                       "~/Content/datatables/css/datatables.bootstrap.css",
                       "~/Content/site.css",
-                      "~/Content/typeahead.css"
+                      "~/Content/typeahead.css",
+                      "~/Content/toastr.css"
                      ));
         }
     }
